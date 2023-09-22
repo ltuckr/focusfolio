00216c6b7c7909fa1b3492ba56a566d8399862b7
Lauren Tucker <l.tucker.rdh@gmail.com>
2023-09-21 21:04:31 -0400

    added index.js to models

--- server/models/index.js
+++ server/models/index.js
@@ -1,4 +1,33 @@
-const Tech = require('./Tech');
-const Matchup = require('./Matchup');
+// models/index.js
 
-module.exports = { Tech, Matchup };
+const mongoose = require('mongoose');
+
+// Import individual models
+const User = require('./User');
+const Project = require('./Project');
+const Favorite = require('./Favorite');
+const Comment = require('./Comment');
+const Purchase = require('./Purchase');
+
+// Connect to the MongoDB database (if not already connected)
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/focusfolio', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB database');
+});
+
+module.exports = {
+  User,
+  Project,
+  Favorite,
+  Comment,
+  Purchase,
+};
