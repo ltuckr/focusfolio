1bea2b828d0fac31931b7d04f135f7d0b3a29445
devramirez <aramirezdev1@gmail.com>
2023-09-25 19:01:24 -0400

    configured schemas files - resolvers, typeDefs

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,29 +1,46 @@
-const { Tech, Matchup } = require('../models');
+const { User, Project, Purchase, Favorite, Comment } = require('../models');
 
 const resolvers = {
   Query: {
-    tech: async () => {
-      return Tech.find({});
+    users: async () => {
+      return User.find({});
     },
-    matchups: async (parent, { _id }) => {
-      const params = _id ? { _id } : {};
-      return Matchup.find(params);
+    projects: async () => {
+      return Project.find({});
+    },
+    purchases: async () => {
+      return Purchase.find({});
+    },
+    favorites: async () => {
+      return Favorite.find({});
+    },
+    comments: async () => {
+      return Comment.find({});
     },
   },
   Mutation: {
-    createMatchup: async (parent, args) => {
-      const matchup = await Matchup.create(args);
-      return matchup;
+    createUser: async (parent, args) => {
+      const user = await User.create(args);
+      return user;
     },
-    createVote: async (parent, { _id, techNum }) => {
-      const vote = await Matchup.findOneAndUpdate(
-        { _id },
-        { $inc: { [`tech${techNum}_votes`]: 1 } },
-        { new: true }
-      );
-      return vote;
+    createProject: async (parent, args) => {
+      const project = await Project.create(args);
+      return project;
+    },
+    createPurchase: async (parent, args) => {
+      const purchase = await Purchase.create(args);
+      return purchase;
+    },
+    createFavorite: async (parent, args) => {
+      const favorite = await Favorite.create(args);
+      return favorite;
+    },
+    createComment: async (parent, args) => {
+      const comment = await Comment.create(args);
+      return comment;
     },
   },
 };
+
 
 module.exports = resolvers;
