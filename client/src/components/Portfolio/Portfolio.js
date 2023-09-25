36c6a00cea164f90b3e7b9a2d8d8d72631b310c8
IanPomaz <128009980+IanPomaz@users.noreply.github.com>
2023-09-25 19:52:00 -0400

    starter folder structure

---
+++ client/src/components/Portfolio/Portfolio.js
@@ -0,0 +1,16 @@
+import './Portfolio.css'
+
+const Portfolio = () =>
+    <div className='header'>
+        <h1>FocusFolio</h1>
+
+        <div>
+            <button>Home</button>
+            <button>Portfolio</button>
+            <button>Gallery</button>
+            <button>Contact Me</button>
+            <button>Cart</button>
+        </div>
+    </div>
+
+export default Portfolio;
