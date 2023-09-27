3c0cf87731530313883e89bfae29740f530a7ac1
Christina2208 <christinalf22@gmail.com>
2023-09-27 18:53:13 -0400

    added image galleries to pages and components

--- client/src/pages/gallery.js
+++ client/src/pages/gallery.js
@@ -1,9 +1,11 @@
 import styles from "./gallery.module.css";
+import GalleryImgs from "../components/imageGallery"
 
 export default function Gallery(){
     return(
         <>
-        <h1>Gallery</h1>
+        <h1>Client Gallery Images</h1>
+        <GalleryImgs />
         </>
     )
 }
