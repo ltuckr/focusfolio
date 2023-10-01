16cd6ead8912426c3d2fcbe2cfd8f7e6cc261cf0
Lauren <128933116+ltuckr@users.noreply.github.com>
2023-10-01 16:28:22 -0400

    Update gallery.js

--- client/src/pages/gallery.js
+++ client/src/pages/gallery.js
@@ -1,9 +1,10 @@
 import styles from "./gallery.module.css";
 import GalleryImgs from "../components/imageGallery"
-<h1>Client Gallery Images</h1>
+
 export default function Gallery(){
     return(
         <>
+        <h1>Client Gallery Images</h1>
         <GalleryImgs />
         </>
     )
