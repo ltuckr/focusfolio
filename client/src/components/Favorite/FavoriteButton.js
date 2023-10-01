26ec72bcbd9ed48c780e935e6d94a33cd2780121
Christina2208 <christinalf22@gmail.com>
2023-10-01 17:37:09 -0400

    added the heart inside the img

--- client/src/components/Favorite/FavoriteButton.js
+++ client/src/components/Favorite/FavoriteButton.js
@@ -3,45 +3,51 @@ import { useMutation } from "@apollo/client";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart } from "@fortawesome/free-solid-svg-icons";
 import { ADD_FAVORITE, REMOVE_FAVORITE } from "../../utils/mutations"; // Updated import statements
+import styles from "../ImageGallery/imageGallery.module.css";
+
 
 function FavoriteButton({ userId, imageUrl, isFavorited, onToggleFavorite }) {
-  const [addFavorite] = useMutation(ADD_FAVORITE);
-  const [removeFavorite] = useMutation(REMOVE_FAVORITE);
+const [addFavorite] = useMutation(ADD_FAVORITE);
+const [removeFavorite] = useMutation(REMOVE_FAVORITE);
 
-  const handleFavorite = async () => {
-    try {
-      if (!isFavorited) {
-        // If not favorited, add the favorite
-        await addFavorite({
-          variables: {
-            imageId: imageUrl,
-          },
-        });
-      } else {
-        // If already favorited, remove the favorite
-        await removeFavorite({
-          variables: {
-            imageId: imageUrl,
-          },
-        });
-      }
 
-      // Notify the parent component about the favorite status change
-      onToggleFavorite(!isFavorited);
-    } catch (error) {
-      console.error("Error toggling favorite:", error);
-    }
-  };
-
-  return (
-    <button onClick={handleFavorite}>
-      {isFavorited ? (
-        <FontAwesomeIcon icon={faHeart} color="red" />
-      ) : (
-        <FontAwesomeIcon icon={faHeart} color="gray" />
-      )}
-    </button>
-  );
+const handleFavorite = async () => {
+try {
+if (!isFavorited) {
+// If not favorited, add the favorite
+await addFavorite({
+variables: {
+imageId: imageUrl,
+},
+});
+} else {
+// If already favorited, remove the favorite
+await removeFavorite({
+variables: {
+imageId: imageUrl,
+},
+});
 }
+
+
+// Notify the parent component about the favorite status change
+onToggleFavorite(!isFavorited);
+} catch (error) {
+console.error("Error toggling favorite:", error);
+}
+};
+
+
+return (
+<button onClick={handleFavorite} className={styles.favoriteButtonContainer} >
+{isFavorited ? (
+<FontAwesomeIcon icon={faHeart} color="red" />
+) : (
+<FontAwesomeIcon icon={faHeart} color="white" />
+)}
+</button>
+);
+}
+
 
 export default FavoriteButton;
--- client/src/components/ImageGallery/imageGallery.js
+++ client/src/components/ImageGallery/imageGallery.js
@@ -4,6 +4,7 @@ import styles from "./imageGallery.module.css";
 import { ADD_FAVORITE, REMOVE_FAVORITE } from "../../utils/mutations"; // Import the mutations from your file
 import FavoriteButton from "../Favorite/FavoriteButton";
 
+
 // Image Imports
 import NB1Image from "../../images/NB1.jpg";
 import NB2Image from "../../images/NB2.jpg";
@@ -12,74 +13,91 @@ import NB4Image from "../../images/NB4.jpg";
 import NB5Image from "../../images/NB5.jpg";
 import NB6Image from "../../images/NB6.jpg";
 
+
 // Image data
 const images = [
-  NB1Image,
-  NB2Image,
-  NB3Image,
-  NB4Image,
-  NB5Image,
-  NB6Image,
+NB1Image,
+NB2Image,
+NB3Image,
+NB4Image,
+NB5Image,
+NB6Image
 ];
 
+
+
 const GalleryImgs = () => {
-  const [selectedImage, setSelectedImage] = useState(null);
-  const [isImageFavorited, setIsImageFavorited] = useState(false);
+const [selectedImage, setSelectedImage] = useState(null);
+const [isImageFavorited, setIsImageFavorited] = useState(false);
+const [commentText, setCommentText] = useState("");
 
 
-  const openModal = (image) => {
-    setSelectedImage(image);
-    setIsImageFavorited(false);
-   
-  };
-
- 
-
-  const [addFavorite] = useMutation(ADD_FAVORITE);
-  const [removeFavorite] = useMutation(REMOVE_FAVORITE);
-
-  const handleFavorite = async () => {
-    try {
-      if (!isImageFavorited) {
-        // If not favorited, add the favorite
-        await addFavorite({
-          variables: {
-            imageId: selectedImage.replace(/\.[^/.]+$/, ""), // Remove file extension from image name
-          },
-        });
-      } else {
-        // If already favorited, remove the favorite
-        await removeFavorite({
-          variables: {
-            imageId: selectedImage.replace(/\.[^/.]+$/, ""), // Remove file extension from image name
-          },
-        });
-      }
-
-      setIsImageFavorited(!isImageFavorited); // Toggle the favorite status
-    } catch (error) {
-      console.error("Error toggling favorite:", error);
-    }
-  };
-
-  return (
-    <div className={styles.gallery}>
-      {images.map((image, index) => (
-        <div key={index} className={styles.galleryItem}>
-          <img
-            src={image}
-            alt={`Image ${index}`}
-            onClick={() => openModal(image)}
-          />
-          <FavoriteButton
-            imageUrl={image}
-            isFavorited={isImageFavorited}
-            onToggleFavorite={handleFavorite}
-          />
-        </div>
-      ))}
-    </div>
-  );
+const openModal = (image) => {
+setSelectedImage(image);
+setIsImageFavorited(false);
+setCommentText("");
 };
+
+
+const closeModal = () => { //modal issue?
+setSelectedImage(null);
+};
+
+
+const [addFavorite] = useMutation(ADD_FAVORITE);
+const [removeFavorite] = useMutation(REMOVE_FAVORITE);
+
+
+const handleFavorite = async () => {
+try {
+if (!isImageFavorited) {
+// If not favorited, add the favorite
+await addFavorite({
+variables: {
+imageId: selectedImage,
+},
+});
+} else {
+// If already favorited, remove the favorite
+await removeFavorite({
+variables: {
+imageId: selectedImage,
+},
+});
+}
+
+
+setIsImageFavorited(!isImageFavorited); // Toggle the favorite status
+} catch (error) {
+console.error("Error toggling favorite:", error);
+}
+};
+
+
+return (
+<div className={styles.gallery}>
+{images.map((image, index) => (
+<div key={index} className={styles.galleryItem}>
+<div className={styles.imageContainer}>
+<img
+src={image}
+alt={`Image ${index}`}
+onClick={() => openModal(image)}
+className={styles.imageItem}
+/>
+
+
+<FavoriteButton className={styles.favoriteButtonContainer}
+imageUrl={image}
+isFavorited={isImageFavorited}
+onToggleFavorite={handleFavorite}
+/>
+</div>
+</div>
+))}
+</div>
+);
+};
+
 
 export default GalleryImgs;
