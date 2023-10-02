a1d6abf15db4011b7526126010042c8681e0d15c
Lauren Tucker <l.tucker.rdh@gmail.com>
2023-10-02 18:09:07 -0400

    Stefan again - index and server

--- server/models/index.js
+++ server/models/index.js
@@ -1,17 +1,11 @@
 
-const mongoose = require('mongoose');
 
 // Import individual models
 const User = require('./User');
 const Image = require('./Image');
 const Favorite = require('./Favorite');
 
-const db = mongoose.connection;
 
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once('open', () => {
-  console.log('Connected to MongoDB database');
-});
 
 module.exports = {
   User,
